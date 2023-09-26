66c1151568326b6d338c2731173426c4e2135477
Monika Verma <monika.verma@taskcraft.in>
2023-09-26 17:25:26 +0530

    updates with deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,73 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.3"
-
-set :application, "my_app_name"
-
-# Define your server's IP or hostname, SSH user, and SSH key
-server '3.7.45.234', user: 'ubuntu', roles: %w{app db web}
-
-# Specify your application's repository URL and branch
 set :repo_url, 'https://github.com/monikavermaa/demo_taskcraft_app.git'
-set :branch, 'master'
+set :application,     'EduParc'
+set :user,            'ec2-user'
+set :puma_threads,    [4, 16]
+set :puma_workers,    6
+# Don't change these unless you know what you're doing
+set :pty,             false
+set :use_sudo,        false
+set :rvm_ruby_string, '2.5.3@eduparc' # you probably have this already
+set :deploy_via,      :remote_cache
 
-# Define the directory where your application will be deployed
-#set :deploy_to, '/var/www/your-app-name'
-set :deploy_to, 'home/ubuntu/var/www/my-rails-project'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
+#set :deploy_to, 'home/ubuntu/var/www/my-rails-project'
+set :deploy_to, '/var/www/my-rails-project/#{fetch(:application)}'
+# set :deploy_to,       "/var/deploy/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false  # Change to false when not using ActiveRecord
+set :bundle_flags, '--deployment'
+# set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
+# set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
+​
+## Defaults:
+# set :scm,           :git
+set :branch,        :staging
+# set :format,        :pretty
+# set :log_level,     :debug
 # set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{.env}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+append :linked_dirs,  "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "bundle"
+​
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+​
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+​
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+​
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  # after  :finishing,    :restart
+end
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
