b44834aecf4b379ec3f362d5aa8bb9ab676804e9
Monika Verma <monika.verma@taskcraft.in>
2023-09-27 08:42:22 +0530

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,12 +19,9 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log,  "#{release_path}/log/puma.error.log"
-# set :ssh_options,     { keys: "/home/monika/.ssh/id_rsa" }
-# set :ssh_options, {
-#     keys: %w(~/.ssh/id_rsa),
-#     forward_agent: false
-#  }
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :ssh_options,     { keys: "/home/monika/.ssh/id_rsa" }
+
+#set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to false when not using ActiveRecord
@@ -35,12 +32,6 @@ set :bundle_flags, '--deployment'
 ## Defaults:
 # set :scm,           :git
 set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{.env}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 append :linked_dirs,  "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "bundle"
 
 namespace :deploy do
