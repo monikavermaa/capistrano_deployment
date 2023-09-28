77dffb65b7481c92117113679467fd6b5bde9523
Monika Verma <monika.verma@taskcraft.in>
2023-09-28 13:07:18 +0530

    try remove webpack might need revert..

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module MyRailsProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Remove the following line, which includes Webpacker.
+    # If you want to use the Asset Pipeline exclusively, you don't need it.
+    # config.require_master_key = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +22,11 @@ module MyRailsProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Disable the asset pipeline, since you won't be using Webpacker.
+    config.assets.enabled = false
+
+    # Uncomment the following line if you want to serve static assets from the public folder.
+    # config.public_file_server.enabled = true
   end
 end
