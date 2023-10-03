6c20f6f2857d4db65b6d2e102e63d67ea6a9e230
Monika Verma <monika.verma@taskcraft.in>
2023-10-03 12:20:15 +0530

    fix issue..

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '2.43.204.148', user: 'ubuntu', roles: [:web, :app, :db], primary: true, ssh_options: { forward_agent: true }
+server '43.204.148.27', user: 'ubuntu', roles: [:web, :app, :db], primary: true, ssh_options: { forward_agent: true }
 set :rails_env, :production
 set :rack_env,  :production
 set :stage,     :production
