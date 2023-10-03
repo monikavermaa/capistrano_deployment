6fe1a047d6d5ee0720d0ef1dfb380ddf87310bcc
Monika Verma <monika.verma@taskcraft.in>
2023-10-03 12:16:08 +0530

    change IP..

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
-
-server '3.7.45.234', user: 'ubuntu', roles: [:web, :app, :db], primary: true, ssh_options: { forward_agent: true }
+server '2.43.204.148', user: 'ubuntu', roles: [:web, :app, :db], primary: true, ssh_options: { forward_agent: true }
 set :rails_env, :production
 set :rack_env,  :production
 set :stage,     :production
